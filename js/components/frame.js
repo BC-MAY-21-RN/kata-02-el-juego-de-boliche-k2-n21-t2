56a1b45dc5113d28eb4dcdd7ae1ed22eaf19155a
RobertoCarMed <betocarrasco3652@gmail.com>
2021-11-25 16:15:17 -0600

    initial commit

---
+++ js/components/frame.js
@@ -0,0 +1,9 @@
+export default class Frame {
+  constructor() {
+    // Aqui van las propiedades
+    // Cada Frame
+  }
+
+  // Aqui van los metodos
+
+}
---
+++ js/components/lanzamiento.js
@@ -0,0 +1,11 @@
+export default class Lanzamiento {
+  constructor() {
+    // Aqui las propiedades
+    // Un lanzamiento puede derribar hasta 10 pines pero no se pueden derribar mas de 10 pines en dos lanzamientos
+    // Cada lanzamiento debe recibir los puntos del lanzamiento anterior (para que el puntaje total entre los dos lanzamientos no supere el 10 en puntaje)
+
+  }
+
+  // Aqui van los metodos
+
+}
